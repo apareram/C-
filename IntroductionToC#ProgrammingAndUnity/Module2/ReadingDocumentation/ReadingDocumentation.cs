1a924f248fee1e253441d50fafbf4b31ef978ec0
aparera <aparera@sasa-mexico.com>
2024-09-05 12:05:25 -0600

    finished the lecture on reading documentation

---
+++ IntroductionToC#ProgrammingAndUnity/Module2/ReadingDocumentation/ReadingDocumentation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReadingDocumentation{
+    /// <summary>
+    /// Reading Documentation lecture code
+    /// </summary>
+    internal class Program{
+        /// <summary>
+        /// Demonstrates reading documentation
+        /// </summary>
+        /// <param name="args">command-lines args</param>
+        static void Main(string[] args){
+            // calculate the cosine of 45 degrees
+            // float cosine = (float)Math.Cos(45 * Math.PI / 180);
+            float cosine = MathF.Cos(45 * MathF.PI / 180);
+            Console.WriteLine("Cosine of 45: " + cosine);
+        }
+    }
+}
