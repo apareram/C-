954cc2f096df716899238040fcd86d26997b5778
aparera <aparera@sasa-mexico.com>
2024-09-10 19:45:09 -0600

    code for the using the constructor lecture

---
+++ IntroductionToC#ProgrammingAndUnity/Module3/UsingClass/UsingDeckClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UsingDeckClass{
+    /// <summary>
+    /// Class and objects lecture code
+    /// </summary>
+    internal class Program{
+        /// <summary>
+        /// Demonstrates using a deck og cards 
+        /// </summary>
+        /// <param name="args">command-lines args</param>
+        static void Main(string[] args){
+            // declare and print a deck of cards
+            Deck deck = new Deck();
+            deck.Print();
+        }
+    }
+}
